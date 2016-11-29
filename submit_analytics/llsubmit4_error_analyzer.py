62e91513cb3c10288af31c6f7a7645d7d8407a2b
perillaroc <perillaroc@gmail.com>
2016-11-29 21:41:35 +0800

    add hour/weekday grid analytics.

--- submit_analytics/llsubmit4_error_analyzer.py
+++ submit_analytics/llsubmit4_error_analyzer.py
@@ -205,6 +205,96 @@ def count_handler(args):
     print(json.dumps(result, indent=4))
 
 
+def get_value(record, name):
+    if name == 'day':
+        return record['date'].strftime("%Y-%m-%d")
+    elif name == 'weekday':
+        return record['date'].weekday()
+    elif name == 'system':
+        return get_system_from_path(record['info']['path'])
+    elif name == 'date-hour':
+        cur_datetime = record['date']
+        cur_date = cur_datetime.date()
+        zero_time = datetime.time(cur_datetime.hour)
+        cur_hour = datetime.datetime.combine(cur_date, zero_time)
+        return cur_hour.strftime("%Y-%m-%d %H:%M:%S")
+    elif name == 'hour':
+        return record['date'].strftime("%H")
+    else:
+        raise Exception('name unsupported', name)
+
+
+def grid_handler(args):
+    begin_date = args.begin_date
+    end_date = args.end_date
+    log_file_path = args.log_file_path
+    x_type = args.x_type
+    y_type = args.y_type
+
+    if begin_date:
+        begin_date = datetime.datetime.strptime(begin_date, "%Y-%m-%d")
+    if end_date:
+        end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+
+    grid_result = dict()
+    try:
+        with open(log_file_path, 'r') as log_file:
+            for line in log_file:
+                record = parse_error_log(line)
+                # check date
+                if begin_date:
+                    if record['date'].date() < begin_date.date():
+                        continue
+                if end_date:
+                    if record['date'].date() >= end_date.date():
+                        continue
+
+                x_value = get_value(record, x_type)
+                y_value = get_value(record, y_type)
+
+                if x_value not in grid_result:
+                    grid_result[x_value] = dict()
+                if y_value not in grid_result[x_value]:
+                    grid_result[x_value][y_value] = 0
+                grid_result[x_value][y_value] += 1
+
+    except FileNotFoundError as e:
+        result = {
+            'app': 'llsubmit4_error_analyzer',
+            'type': 'grid',
+            'error': 'file_not_found',
+            'timestamp': datetime.datetime.now().timestamp(),
+            'data': {
+                'error_message': 'file is not found',
+                'request': {
+                    'log_file_path': log_file_path,
+                }
+            }
+        }
+        print(json.dumps(result))
+        return
+
+    result = {
+        'app': 'llsubmit4_error_analyzer',
+        'type': 'grid',
+        'timestamp': datetime.datetime.now().timestamp(),
+        'data': {
+            'response': {
+                'grid_result': grid_result,
+            },
+            'request': {
+                'log_file_path': log_file_path,
+                'x_type': x_type,
+                'y_type': y_type,
+                'begin_date': begin_date.strftime('%Y-%m-%d'),
+                'end_date': end_date.strftime('%Y-%m-%d'),
+            }
+        }
+    }
+
+    print(json.dumps(result, indent=4))
+
+
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -226,8 +316,18 @@ def main():
     count_parser.add_argument("--begin-time", help="begin time, hh:mm:ss")
     count_parser.add_argument("--end-time", help="end time, hh:mm:ss")
     count_parser.add_argument("--type", dest="count_type", help="count type", required=True,
-                              choices=['day', 'weekday', 'date-hour', 'hour', 'system'])
+                              choices=['day', 'weekday', 'date-hour', 'hour', 'system', 'hour/weekday'])
     count_parser.set_defaults(func=count_handler)
+
+    grid_parser = sub_parsers.add_parser('grid', description="get grid result.")
+    grid_parser.add_argument("-f", "--file", help="log file path", dest="log_file_path", required=True)
+    grid_parser.add_argument("--begin-date", help="begin date, YYYY-MM-DD")
+    grid_parser.add_argument("--end-date", help="end date, YYYY-MM-DD")
+    grid_parser.add_argument("--begin-time", help="begin time, hh:mm:ss")
+    grid_parser.add_argument("--end-time", help="end time, hh:mm:ss")
+    grid_parser.add_argument("--x-type", dest="x_type", help="x axis type", required=True, choices=['hour'])
+    grid_parser.add_argument("--y-type", dest="y_type", help="y axis type", required=True, choices=['weekday'])
+    grid_parser.set_defaults(func=grid_handler)
 
     args = parser.parse_args()
     args.func(args)
