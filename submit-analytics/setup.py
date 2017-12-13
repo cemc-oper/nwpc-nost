1fd9f0a8e840d2811e24c733843c592050fd6a62
perillaroc <perillaroc@gmail.com>
2017-12-13 10:09:49 +0800

    make submit_analytics to a python package.

---
+++ submit-analytics/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='nwpc-submit-analytics',
+
+    version='0.1.0',
+
+    description='NWPC submit analytics',
+    long_description=long_description,
+
+    url='https://github.com/perillaroc/nwpc-operation-system-tool',
+
+    author='perillaroc',
+    author_email='perillaroc@gmail.com',
+
+    license='GPL-3.0',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    keywords='nwpc submit analytics',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    include_package_data=True,
+
+    install_requires=[
+        'click'
+    ],
+
+    # extras_require={
+    #     'test': ['pytest'],
+    # },
+
+    entry_points={
+        'console_scripts': [
+            'llsubmit4_error_analyzer=nwpc_submit_analytics.llsubmit4_error_analyzer:cli'
+        ]
+    }
+)
