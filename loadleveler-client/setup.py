2b7e21fec88ea5f5ca63ac7e4921df5445359a17
perillaroc <perillaroc@gmail.com>
2018-12-31 16:20:37 +0800

    :package: update setup.py.

--- loadleveler-client/setup.py
+++ loadleveler-client/setup.py
@@ -25,8 +25,9 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6'
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
 
     keywords='nwpc loadleveler client',
